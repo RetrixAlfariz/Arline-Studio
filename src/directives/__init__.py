eb111cd2a74dad1749ec8aa79c82701f472cf2c6
Rafzan <86295808+RetrixAlfariz@users.noreply.github.com>
2026-08-20 21:45:34 +0700

    temp: wire v1.2.5 command runtime

--- src/directives/__init__.py
+++ src/directives/__init__.py
@@ -3,11 +3,25 @@ from .engine import (
     DYNAMIC_REFERENCES,
     REFERENCE_SELECTORS,
     CommandSpec,
+)
+from .runtime import (
+    COMMAND_RUNTIME_VERSION,
+    RETRIEVAL_PROFILES,
+    ArgSpec,
     DirectiveEngine,
     DirectiveIntent,
+    OptionSpec,
 )
 
 __all__ = [
-    "COMMAND_REGISTRY_VERSION", "DYNAMIC_REFERENCES", "REFERENCE_SELECTORS",
-    "CommandSpec", "DirectiveEngine", "DirectiveIntent",
+    "COMMAND_REGISTRY_VERSION",
+    "COMMAND_RUNTIME_VERSION",
+    "DYNAMIC_REFERENCES",
+    "REFERENCE_SELECTORS",
+    "RETRIEVAL_PROFILES",
+    "ArgSpec",
+    "OptionSpec",
+    "CommandSpec",
+    "DirectiveEngine",
+    "DirectiveIntent",
 ]
